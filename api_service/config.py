5305fbbcc14b646e19fe36329faceb371c41b00a
Angelpperez <angelppgml@gmail.com>
2026-02-15 23:06:31 -0300

    fonts and name

--- api_service/config.py
+++ api_service/config.py
@@ -10,7 +10,7 @@ class Settings(BaseSettings):
 
     host: str = "0.0.0.0"
     port: int = 8000
-    ui_title: str = "Prysm Monitor"
+    ui_title: str = "Prysmian Monitor"
     status_refresh_ms: int = 1000
     vision_service_url: str = "http://localhost:8001"
     vision_public_url: Optional[str] = None
